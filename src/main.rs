808915c2765805bf3a43ec9f28c43a36518244ee
K. Adam Christensen <pope@shifteleven.com>
2023-07-12 19:48:15 -0700

    Sample of reading a cbz file.
    Reads for the first image file and then prints how many bytes it
    contains.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+use std::fs::File;
+use std::path::Path;
+use zip::ZipArchive;
+
+#[derive(Parser)]
+struct Args {
+	/// The location of a .cbz file.
+	cbz_file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+	let args = Args::parse();
+	let fname = Path::new(&args.cbz_file);
+	let zipfile = File::open(fname).unwrap();
+	let mut archive = ZipArchive::new(zipfile).unwrap();
+
+	let first = archive
+		.file_names()
+		.filter(|f| f.ends_with(".jpeg"))
+		.reduce(|res, f| if f < res { f } else { res })
+		.unwrap()
+		.to_owned();
+	println!("First image: {}", first);
+
+	let yeah = archive.by_name(&first).unwrap();
+	println!("It is {}b big", yeah.size());
 }
